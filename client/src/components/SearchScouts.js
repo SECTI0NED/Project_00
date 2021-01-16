08ebc0def9e671d42bd830069d522e3a7a481d70
Nikita Phung <nikitaphung2000@gmail.com>
2021-01-16 11:42:10 +1100

    Worked on styling search bar

--- client/src/components/SearchScouts.js
+++ client/src/components/SearchScouts.js
@@ -1,9 +1,17 @@
-import React from 'react'
 
-export default function SearchScouts() {
-    return (
-        <>
-            <h2>Search Scouts</h2>
-        </>
-    )
+import React, { Component } from 'react'
+import {Form} from "react-bootstrap/esm"
+export default class SearchScouts extends Component {
+    render() {
+        return (
+            <div className="card bg-dark">
+                <div className="search">
+                    <div className="search-bar">
+                        <Form.Control type="text"></Form.Control>
+                    </div>     
+                    <button className="sort-btn"><b>Sort By</b></button>         
+                </div>
+            </div>
+        )
+    }
 }
